aca3104a4a9c9bed5137f7cfa8baecbf999a0e05
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-13 19:34:49 -0300

    feat: added footer info

---
+++ src/constants/footer.ts
@@ -0,0 +1,18 @@
+export const features: string[] = [
+  "Link Shortening",
+  "Branded Links",
+  "Analytics",
+];
+
+export const resources: string[] = [
+  "Blog",
+  "Developers",
+  "Support",
+];
+
+export const company: string[] = [
+  "About",
+  "Our Team",
+  "Careers",
+  "Contact",
+];
