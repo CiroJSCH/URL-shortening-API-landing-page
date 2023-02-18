dbe8c0488bd2bc37b9f7f79428f1da76a433b13d
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-18 11:38:29 -0300

    CTA component

---
+++ src/components/Cta.tsx
@@ -0,0 +1,13 @@
+// Components
+import Button from "@/components/shared/Button";
+
+const Cta = () => {
+  return (
+    <section className="bg-boost-mobile bg-primary-violet h-[300px] bg-cover bg-no-repeat flex flex-col items-center justify-center">
+      <h3 className="text-white text-[27px] font-bold mb-4">Boost your links today</h3>
+      <Button label="Get Started"/>
+    </section>
+  )
+}
+
+export default Cta
