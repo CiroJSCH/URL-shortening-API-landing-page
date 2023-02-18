1ef6ec7212c79f5c0814b54b0f3d62be888a2e45
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-18 19:51:07 -0300

    Responsive CTA

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -3,8 +3,8 @@ import Button from "@/components/shared/Button";
 
 const Cta = () => {
   return (
-    <section className="bg-boost-mobile bg-primary-violet h-[300px] bg-cover bg-no-repeat flex flex-col items-center justify-center">
-      <h3 className="text-white text-[27px] font-bold mb-4">Boost your links today</h3>
+    <section className="bg-boost-mobile xl:bg-boost-desktop bg-primary-violet h-[300px] bg-cover bg-no-repeat flex flex-col items-center justify-center">
+      <h3 className="text-white text-[27px] xl:text-[38px] font-bold mb-4 xl:mb-6">Boost your links today</h3>
       <Button label="Get Started"/>
     </section>
   )
