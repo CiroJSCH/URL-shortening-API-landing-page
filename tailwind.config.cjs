b44fb291e9f45df8967045ba88f88fa31b5f0bf7
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-02-18 15:27:43 -0300

    fixed color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,7 @@ module.exports = {
         'primary-cyan': 'hsl(180, 66%, 49%)',
         'primary-violet': 'hsl(257, 27%, 26%)',
         secondary: 'hsl(0, 87%, 67%)',
-        'gray-500': 'hsl(0, 0%, 75%)',
+        'gray-500': 'hsl(230, 25%, 95%)',
         'gray-600': 'hsl(257, 7%, 63%)',
         'gray-700': 'hsl(255, 11%, 22%)',
         'gray-800': 'hsl(260, 8%, 14%)',
